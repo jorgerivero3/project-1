5121cf516adae785ad10cb0164f3edb236b771e7
Vincent Liu <hliu38@hotmail.com>
2018-10-07 18:19:14 -0500

    fixed broken things so that the python app can at least run

--- Application/models.py
+++ Application/models.py
@@ -1,4 +1,4 @@
-from Application import db
+from Application import db, login_manager
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin): #possibly link to character?
@@ -6,7 +6,7 @@ class User(db.Model, UserMixin): #possibly link to character?
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
-	characters = db.relationship('Character', backref='', eager=True)
+	characters = db.relationship('Character', backref='username', lazy=True) #eager=True) need to figure this out
 
 	'''
 	def get_reset_token(self, expires_sec=1800):
@@ -21,14 +21,18 @@ class User(db.Model, UserMixin): #possibly link to character?
 		except:
 			return None
 		return User.query.get(user_id)
-
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 	'''
 
-class Character(db.model):
+	def __repr__(self):
+		return f"User('{self.username}')"
+
+class Character(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
 	characterName = db.Column(db.String(15), nullable=False)
 	health = db.Column(db.Integer, nullable=False, default=100)
 	alive = db.Column(db.Boolean, nullable=False, default=True)
 	progress = db.Column(db.Integer, nullable=False, default=0)
+	user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 		
+	def __repr__(self):
+		return f"Character('{self.characterName}', '{self.progress}')"
