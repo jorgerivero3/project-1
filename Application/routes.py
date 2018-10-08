87131dfccc2de715c83c58e830ce44846a277148
Vincent Liu <hliu38@hotmail.com>
2018-10-07 20:39:27 -0500

    adding some backend for game running

--- Application/routes.py
+++ Application/routes.py
@@ -9,14 +9,14 @@ import os
 @application.route('/')
 def home():
 	if current_user.is_authenticated:
-		return redirect(url_for('start_game'))
+		return redirect(url_for('game'))
 	return render_template('/home.html', title='The UT Trail')
 
 
 @application.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
-		return redirect(url_for('start_game'))
+		return redirect(url_for('game'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -31,14 +31,14 @@ def register():
 @application.route('/login', methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
-		return redirect(url_for('start_game'))
+		return redirect(url_for('game'))
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
 			login_user(user, remember=form.remember.data)
 			next_page = request.args.get('next')
-			return redirect(next_page) if next_page else redirect(url_for('start_game'))
+			return redirect(url_for('game'))
 		else:
 			flash('Login unsuccessful. Email and/or password incorrect.')
 	return render_template('login.html', title='Login', form=form)
@@ -47,13 +47,6 @@ def login():
 @application.route('/about')
 def about():
 	return render_template('/about.html', title='About The UT Trail')
-
-
-@application.route('/start_game')
-def start():
-	if current_user.is_authenticated:
-		return redirect(url_for('game'))
-	return render_template('UTtrailGame.html', title='Gone to Texas')
 
 
 @application.route('/logout')
@@ -94,8 +87,31 @@ def reset_token(token):
 
 
 ### GAME CODE ###
+@application.route('/game')
+@login_required
+def game():
+	return render_template('UTtrailGame.html', title='Gone to Texas', progress=current_user.progress)
+
+
 @application.route('/game/<progress>', methods=['GET', 'POST'])
-def game(progress):
-	if current_user.is_authenticated:
-		return render_template('game.html', title='hookem', progress=current_user.progress)
-	return render_template('game.html', title='hookem')
+@login_required
+def gameplay(progress):
+	form = GameInput()
+	if form.validate_on_submit():
+		if form.input.data == 1:
+			current_user.health = current_user.health - 5
+		elif form.input.data == 2:
+			current_user.sanity = current_user.sanity - 5
+		elif form.input.data == 3:
+			current_user.grades = current_user.grades - 5
+		current_user.progress = current_user.progress + 1
+		if current_user.progress > 9:
+			return redirect(url_for(gameover))
+		return redirect(url_for(gameplay))
+	return render_template('game.html', title='hookem', progress=current_user.progress)
+
+
+@application.route('/gameover')
+@login_required
+def gameover():
+	return render_template('game.html', health=current_user.health, sanity=current_user.sanity, grades=current_user.grades)
