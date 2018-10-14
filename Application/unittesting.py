d5c57de9d5a6a839386aaae0c2bebecb58fdd0dc
Vincent Liu <hliu38@hotmail.com>
2018-10-13 19:37:13 -0500

    removed __init__ so function can work properly

--- Application/unittesting.py
+++ Application/unittesting.py
@@ -5,26 +5,32 @@ from Application.routes import get_level, doEffect
 
 
 class functiontesting(unittest.TestCase):
-	def __init__():
+	
+	def test_levels():
+		self.assertTrue(len(levels.master != 0)) #non empty dictionary
+	
+	def test_effects(index, pageDetails): #testing function from routes
 		current_user = User() #create object
 		#initialize stats
 		current_user.health = 100
 		current_user.sanity = 100
 		current_user.grades = 100
-		#used for tests 2 & 3
+		#progress/choice
 		index = 0
 		current_user.progress = 'a1'
 		pageDetails = get_level(current_user.progress)
-
-	def test_levels():
-		self.assertTrue(len(levels.master != 0)) #non empty dictionary
-	
-	def test_effects(index, pageDetails): #testing function from routes
+		#function within routes
 		eff = pageDetails.effects[index]
 		doEffect(eff)
 		self.asserEqual(current_user.sanity, 95) #effect specific to choice
 
 	def test_update(index, pageDetails):
+		current_user = User() #create object
+		#progress/choice
+		index = 0
+		current_user.progress = 'a1'
+		pageDetails = get_level(current_user.progress)
+		#function within routes
 		current_user.progress = pageDetails.get_next_level(index)
 		self.asserEqual(current_user.progress, 'a2') #specific to choice
 
